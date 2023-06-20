180e826357217c9d44ff814e50167b767b28cd7b
ashwani kumar <suryanashwin4u@ashwanis-MacBook-Pro.local>
2023-06-20 09:29:34 +0530

    changes

--- is_a_number_prime.java
+++ is_a_number_prime.java
@@ -2,10 +2,15 @@ import java.util.*;
 
 public class is_a_number_prime {
     public static void main(String[] args){
-        System.out.println("Enter Number greater than 1:");
+        System.out.print("Enter any number:");
         Scanner scan = new Scanner(System.in);
         int number = scan.nextInt();
         int count = 0;
+
+        if(number <= 1){
+            System.out.println("This is not a prime number");
+            System.exit(0);
+        }
         
         for(int i = 1; i <= number; i++){
             if(number % i == 0){
