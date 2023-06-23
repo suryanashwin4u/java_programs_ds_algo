5ddd535f303afdebfa2117b896a8d40a42dd849e
ashwani kumar <suryanashwin4u@ashwanis-MacBook-Pro.local>
2023-06-23 20:46:06 +0530

    changes

---
+++ benjamin_bulbs_fluct.java
@@ -0,0 +1,9 @@
+import java.util.Scanner;
+
+public class benjamin_bulbs_fluct {
+    public static void main(String args[]){
+        Scanner scan = new Scanner(System.in);
+        System.out.print("Enter number of bulbs:");
+        int bulbs_number = scan.nextInt();
+        
+}
