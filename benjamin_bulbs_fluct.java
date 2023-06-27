970a39e7f262f618309066b3de2937f793cf25a1
ashwani kumar <suryanashwin4u@ashwanis-MacBook-Pro.local>
2023-06-27 09:06:26 +0530

    changes

--- benjamin_bulbs_fluct.java
+++ benjamin_bulbs_fluct.java
@@ -5,5 +5,13 @@ public class benjamin_bulbs_fluct {
         Scanner scan = new Scanner(System.in);
         System.out.print("Enter number of bulbs:");
         int bulbs_number = scan.nextInt();
-        
+        System.out.print("Enter Voltage Fluctuation Count:");
+        int fluct_number = scan.nextInt();
+
+        for(int i=1; i<= bulbs_number; i++){
+            if(i % fluct_number == 0){
+                System.out.println(i+"th bulb will be turned ON");
+            }
+        }
+    }
 }
