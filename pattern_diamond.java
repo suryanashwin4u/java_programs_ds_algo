14cdfba17508d8943bba4d04652b63b331bbc709
ashwani kumar <suryanashwin4u@ashwanis-MacBook-Pro.local>
2023-06-28 12:51:48 +0530

    changes

---
+++ pattern_diamond.java
@@ -0,0 +1,37 @@
+import java.util.Scanner;
+
+public class pattern_diamond {
+    public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
+
+        System.out.print("Enter number of rows:");
+        int rows = scan.nextInt();
+
+        int space = rows / 2;
+        int star = 1;
+
+        // this loop is for rows
+        for (int i = 1; i <= rows; i++) {
+
+            // this loop is to print spaces in one line
+            for (int j = 1; j <= space; j++) {
+                System.out.print(" ");
+            }
+
+            // this loop is to print stars in one line
+            for (int j = 1; j <= star; j++) {
+                System.out.print("*");
+            }
+
+            if (i <= rows / 2) {
+                space--;
+                star += 2;
+            } else {
+                space++;
+                star -= 2;
+            }
+
+            System.out.println();// to jump at next line
+        }
+    }
+}
