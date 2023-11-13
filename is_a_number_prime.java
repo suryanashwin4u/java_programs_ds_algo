a2b4ace4c6fb4ac35de57d2336b3e5d8294fda86
ashwani kumar <suryanashwin4u@ashwanis-MacBook-Pro.local>
2023-11-13 22:09:54 +0530

    changes done today

--- is_a_number_prime.java
+++ is_a_number_prime.java
@@ -1,27 +1,56 @@
 import java.util.*;
 
+// public class is_a_number_prime {
+//     public static void main(String[] args) {
+//         System.out.print("Enter any number:");
+//         Scanner scan = new Scanner(System.in);
+//         int number = scan.nextInt();
+//         int count = 0;
+
+//         if (number <= 1) {
+//             System.out.println("This is not a prime number");
+//             System.exit(0);
+//         }
+
+//         for (int i = 1; i <= number; i++) {
+//             if (number % i == 0) {
+//                 count++;
+//             }
+//         }
+
+//         if (count == 2) {
+//             System.out.println("This number is a prime number");
+//         } else {
+//             System.out.println("This is not a prime number");
+//         }
+//     }
+// }
+
+//better approach
 public class is_a_number_prime {
-    public static void main(String[] args){
-        System.out.print("Enter any number:");
+    public static void main(String[] args) {
         Scanner scan = new Scanner(System.in);
-        int number = scan.nextInt();
-        int count = 0;
+        System.out.print("Enter the Amount of Numbers:");
+        int amt = scan.nextInt();
 
-        if(number <= 1){
-            System.out.println("This is not a prime number");
-            System.exit(0);
-        }
-        
-        for(int i = 1; i <= number; i++){
-            if(number % i == 0){
-                count++;
+        for (int i = 0; i < amt; i++) {
+            System.out.print("Enter Number:");
+
+            int num = scan.nextInt();
+            int count = 0;
+
+            for (int div = 2; div * div <= num; div++) {
+                if (num % div == 0) {
+                    count++;
+                    break;
+                }
             }
-        }
 
-        if(count == 2){
-            System.out.println("This number is a prime number");
-        }else{
-            System.out.println("This is not a prime number");
+            if (count == 0) {
+                System.out.println("status: prime number");
+            } else {
+                System.out.println("status: not a prime number");
+            }
         }
     }
 }
--- print_all_prime_numbers_between_given_range.java
+++ print_all_prime_numbers_between_given_range.java
@@ -1,34 +1,61 @@
 import java.util.*;
 
+// public class print_all_prime_numbers_between_given_range {
+// public static void main(String args[]){
+// Scanner scan = new Scanner(System.in);
+
+// System.out.print("Enter lower number:");
+// int lowerRange = scan.nextInt();
+
+// System.out.print("Enter higher number:");
+// int higherRange = scan.nextInt();
+
+// for(int number = lowerRange; number <= higherRange; number++){
+// boolean isPrime =
+// print_all_prime_numbers_between_given_range.checkPrime(number);
+
+// if(isPrime){
+// System.out.println(number);
+// }
+// }
+// }
+
+// public static boolean checkPrime(int num){
+// if(num >= 2){
+// for(int i = 2; i < num ; i++){
+// if(num % i == 0){
+// return false;
+// }
+// }
+// }else{
+// return false;
+// }
+// return true;
+// }
+// }
+
+//better approach
 public class print_all_prime_numbers_between_given_range {
-    public static void main(String args[]){
+    public static void main(String[] args) {
         Scanner scan = new Scanner(System.in);
+        System.out.print("Enter low:");
+        int low = scan.nextInt();
+        System.out.print("Enter high:");
+        int high = scan.nextInt();
+        System.out.println("Prime Numbers:");
 
-        System.out.print("Enter lower number:");
-        int lowerRange = scan.nextInt();
-
-        System.out.print("Enter higher number:");
-        int higherRange = scan.nextInt();
-
-        for(int number = lowerRange; number <= higherRange; number++){
-            boolean isPrime = print_all_prime_numbers_between_given_range.checkPrime(number);
-
-            if(isPrime){
-                System.out.println(number);
-            }
-        }
-    }
-    
-    public static boolean checkPrime(int num){
-        if(num >= 2){
-            for(int i = 2; i < num ; i++){
-                if(num % i == 0){
-                    return false;
+        for (int num = low; num <= high; num++) {
+            int count = 0;
+            for (int div = 2; div * div <= num; div++) {
+                if (num % div == 0) {
+                    count++;
+                    break;
                 }
             }
-        }else{
-            return false;
+
+            if (count == 0) {
+                System.out.println(num);
+            }
         }
-        return true;
     }
 }
