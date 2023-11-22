5b90e801d2f20188a59d5530c63f8a330cbd6099
ashwani kumar <suryanashwin4u@ashwanis-MacBook-Pro.local>
2023-11-22 23:49:04 +0530

    changes

--- count_digits_of_a_number_prog_4.java
+++ count_digits_of_a_number_prog_4.java
@@ -1,14 +1,14 @@
 import java.util.Scanner;
 
-public class count_digits_of_a_number {
-    public static void main(String args[]){
+public class count_digits_of_a_number_prog_4 {
+    public static void main(String args[]) {
         Scanner scan = new Scanner(System.in);
         System.out.print("Enter number:");
         int number = scan.nextInt();
 
         int countDigits = 0;
 
-        while(number != 0){
+        while (number != 0) {
             number = number / 10;
             countDigits++;
         }
--- show_digits_of_a_number_prog_5.java
+++ show_digits_of_a_number_prog_5.java
@@ -1,16 +1,17 @@
 import java.util.Scanner;
 
-public class show_digits_of_a_number {
-    public static void main(String args[]){
+public class show_digits_of_a_number_prog_5 {
+    public static void main(String args[]) {
         Scanner scan = new Scanner(System.in);
         System.out.print("Enter number:");
         int number = scan.nextInt();
 
         int digit;
-        while(number != 0){
+
+        while (number != 0) {
             digit = number % 10;
-            System.out.println(digit);
             number = number / 10;
+            System.out.println(digit);
         }
     }
 }
